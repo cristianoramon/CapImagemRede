db42bbff66eacce6247d8ff06cb3feeb7dc4c116
cristianoramon <cristianoramon@yahoo.com.br>
2016-05-17 14:38:53 -0300

    Add files via upload

--- WebCapImagens4.py
+++ WebCapImagens4.py
@@ -3,9 +3,9 @@
 
 """
 Created on Tue Mar 31 08:29:30 2015
-
 @author: ramon
 """
+import argparse
 import re
 import zlib
 #import cv2
@@ -14,11 +14,6 @@ from scapy import  *
 import time
 from datetime import date,datetime
 
-# our packet callback
-def packet_callback(packet): 
-    print(packet.show())
-    
-
 
 
 from threading import Thread
@@ -26,7 +21,7 @@ from Queue import Queue, Empty
 from scapy.all import *
 
 #interface
-m_iface = "wlan0"
+#m_iface = "wlan0"
 m_finished = False
 m_dst = "192.168.0.1"
 
@@ -35,11 +30,14 @@ pictures_directory = "imagens"
 faces_directory    = "faces"
 pcap_file          = "bhp.pcap"
 
+# our packet callback
+def packet_callback(packet):
+    print(packet.show())
+
 def http_assembler(pcap_file):
-  
+
     carved_images   = 0
     faces_detected  = 0
-    
 
     a = pcap_file
     sessions      = a.sessions()
@@ -52,45 +50,43 @@ def http_assembler(pcap_file):
 
                 qt+=1
                 try:
-                    
+
                     if packet[TCP].dport == 80 or packet[TCP].sport == 80:
 
-                        http_payload += str(packet[TCP].payload) 
+                        http_payload += str(packet[TCP].payload)
                 except:
                    pass
-              
-                headers = get_http_headers(http_payload) 
-                
+
+                headers = get_http_headers(http_payload)
+
                 if headers is None:
                     continue
-              
-        image,image_type = extract_image(headers,http_payload) 
-        
 
-        
+        image,image_type = extract_image(headers,http_payload)
+
+
+
         if image is not None and image_type is not None:
-	  
-	    now = datetime.today()    
-	    ano   = now.year
-	    mes   = now.month
-	    dia   = now.day
-	    hora  = now.hour
-	    minu  = now.minute
-	    seg   = now.second
-    
-	    dataFormatada = str(dia) + "_"+str(mes)+"_"+str(ano)+"_hora_"+str(hora)+"_"+str(minu)+"_"+str(seg)
 
+            now = datetime.today()
+            ano   = now.year
+            mes   = now.month
+            dia   = now.day
+            hora  = now.hour
+            minu  = now.minute
+            seg   = now.second
+            dataFormatada = str(dia) + "_"+str(mes)+"_"+str(ano)+"_hora_"+str(hora)+"_"+str(minu)+"_"+str(seg)
             print("\n pic_carver_%s_%d.%s" % (dataFormatada,carved_images,image_type))
             file_name = "pic_carver_%s_%d.%s" % (dataFormatada,carved_images,image_type)
             fd = open("%s/%s" %(pictures_directory,file_name),"wb")
             fd.write(image)
             fd.close()
             carved_images += 1
-            
 
-            
+
+
     return carved_images
-    
+
 
 def get_http_headers(http_payload):
     try:
@@ -123,9 +119,9 @@ def extract_image(headers,http_payload):
                 pass
     except:
         return None,None
-        
+
     return image,image_type
-  
+
 
 def print_summary(pkt):
 
@@ -135,7 +131,21 @@ def print_summary(pkt):
     print("\n Pacote ")
     print("\n")
     carved_images = http_assembler(pkt)
-    print("Extracted: %d images" % carved_images)
+    print("Extraindo : %d images" % carved_images)
+
+def menu():
+
+    parser = argparse.ArgumentParser(
+        description='Captura de Imagens na rede ')
+
+    parser.add_argument(
+        '-i', '--interface', type=str, help='Especificar a interface', required=True)
+
+    args = parser.parse_args()
+    m_iface = args.interface
+
+    return m_iface
+
 
 def iniciaSniffer():
 
@@ -143,23 +153,26 @@ def iniciaSniffer():
     m_finished = False
 
     try:
-	qtPacote = 0
-	while True:
-	    
-	    print("\n Iniciando a captura de pacote qt =%d" % qtPacote ) 
-	    pkt = sniff(iface = m_iface, count = 1, filter = "")
-	    print("\n Termino da captura e iniciando a gravacao do pacote ")        
-	    wrpcap(pcap_file,pkt)
-	    print("\n Leitura do pacote ")
-	    pkt = rdpcap(pcap_file)
-	    print("\n gravando as imagens")
-	    carved_images = http_assembler(pkt)
-	    qtPacote+=1
+        qtPacote = 0
+
+        while True:
+
+            print("\n Iniciando a captura de pacote qt =%d" % qtPacote )
+            pkt = sniff(iface = m_iface, count = 100, filter = "")
+            print("\n Termino da captura e iniciando a gravacao do pacote ")
+            wrpcap(pcap_file,pkt)
+            print("\n Leitura do pacote ")
+            pkt = rdpcap(pcap_file)
+            print("\n gravando as imagens")
+            carved_images = http_assembler(pkt)
+            qtPacote+=1
 
     except KeyboardInterrupt:
         print("\n\nn\ok")
-        carved_images, faces_detected = http_assembler(pkt)
-	print("Extracted: %d images" % carved_images)
+        carved_images = http_assembler(pkt)
+    print("Extraindo: %d images" % carved_images)
 
+m_iface = menu()
 
-iniciaSniffer()
+if m_iface:
+    iniciaSniffer()
